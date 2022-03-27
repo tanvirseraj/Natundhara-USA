a74ae683cbc58850b286c9d48f0c59438a2ec302
Tanvir Seraj <tanvirseraj68@gmail.com>
2022-03-27 23:29:29 +0600

    natundhara-usa

---
+++ script.js
@@ -0,0 +1,37 @@
+
+function w3_open() {
+document.getElementById("mySidebar").style.display = "block";
+}
+function w3_close() {
+document.getElementById("mySidebar").style.display = "none";
+}
+
+
+
+
+
+
+
+
+
+
+
+// function openNav() {
+//     document.getElementById("myNav").style.width = "100%";
+//   }
+  
+//   function closeNav() {
+//     document.getElementById("myNav").style.width = "0%";
+//   }
+  
+//   // Sticky function
+//   $(window).bind('scroll', function() {
+//       if ($(window).scrollTop() > 120) {
+//           $('#Navbar').addClass("sticky");
+//           $('.overlay').css({'top':'40px','width':'0','height':'250px','position':'absolute','overflow-x':'hidden'});
+//       } else {
+//           $('#Navbar').removeClass("sticky");
+//           $('.overlay').css({'top':'150px','width':'0','height':'250px','position':'absolute','overflow-x':'hidden'});
+//       }
+//   });
+  
